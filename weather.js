ec62379fe9dd3979435d3a1f62d1eced600bc6fc
nabeel-w <hassanfarhat986@gmail.com>
2023-03-24 17:15:45 +0530

    first commit

---
+++ weather.js
@@ -0,0 +1,38 @@
+const dataContainer = document.getElementById("data-container");
+const getData = document.getElementById("get-data");
+const apiKey ="d8285afde37227a8da0ebc6892e103e6";
+const unit ="metric";
+const KtoC=(temp)=>{
+    return (temp-273.15).toFixed(2);
+}
+const windDirection =(num)=>{
+    let val=Math.floor((num/22.5)+0.5);
+    arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"];
+    return arr[(val % 16)];
+}
+const successCallback = (position) => {
+    const {latitude,longitude} = position.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(response => {
+           
+           console.log(response);
+           let temp =KtoC(response.main.temp);
+           let city = response.name;
+           let windSpeed = response.wind.speed;
+           let windDir= windDirection(response.wind.deg); 
+           let humidity = response.main.humidity;
+           let description= response.weather[0].main;
+           let postData= document.createElement('div');
+           postData.innerHTML=`<h2>${city}</h2> <p><strong>${description}</strong><br>Temperature-${temp}°C<br> Humidity-${humidity}%<br>Wind Speed-${windSpeed}km/hr ${windDir}</p>`
+           dataContainer.appendChild(postData);
+        }) 
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+
+getData.addEventListener("click",()=>{
+    navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
+});
